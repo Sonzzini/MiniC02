02272119e17e0228962535a76a38a46948bdb73c
Sonzzini <paulo.sonzzini@gmail.com>
2023-08-22 17:28:12 -0300

    Initial Commit

---
+++ MiniC02/MiniC02App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniC02App.swift
+//  MiniC02
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniC02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
