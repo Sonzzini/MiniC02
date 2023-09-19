b2cf5a1b300de6f43638a364f4c65131eb0f4c85
gabrielsabaini <gabriel.tesch@gmail.com>
2023-09-19 17:09:02 -0300

    PostView e HomeView Adicionadas

---
+++ MiniC02/Views/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  MiniC02
+//
+//  Created by Gabriel Sabaini on 19/09/23.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
