01ce0c88e264e4a6fa693a68a48c6eaa0daccb08
Sonzzini <paulo.sonzzini@gmail.com>
2023-09-27 17:31:19 -0300

    Implementar a funcionalidade de POST na API

---
+++ MiniC02/Views/EventPostView.swift
@@ -0,0 +1,18 @@
+//
+//  EventPostView.swift
+//  MiniC02
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 27/09/23.
+//
+
+import SwiftUI
+
+struct EventPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventPostView()
+}
