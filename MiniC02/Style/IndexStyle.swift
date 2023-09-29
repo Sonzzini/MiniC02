2433abcb941a01f795562eb4866e2200fdd83786
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2023-09-29 14:31:30 -0300

    Adicionar os arquivos da tela Onboarding

---
+++ MiniC02/Style/IndexStyle.swift
@@ -0,0 +1,18 @@
+//
+//  IndexStyle.swift
+//  MiniC02
+//
+//  Created by Laura C. Balbachan dos Santos on 28/09/23.
+//
+
+import SwiftUI
+
+struct IndexStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndexStyle()
+}
---
+++ MiniC02/Views/Tutorial2View.swift
@@ -0,0 +1,18 @@
+//
+//  Tutorial2View.swift
+//  MiniC02
+//
+//  Created by Laura C. Balbachan dos Santos on 28/09/23.
+//
+
+import SwiftUI
+
+struct Tutorial2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tutorial2View()
+}
