911488be6d7ad4ef254139eb7e5aea06fdaa8232
gabrielsabaini <gabriel.tesch@gmail.com>
2023-10-09 14:40:27 -0300

    adição de Enum para troca de branch

---
+++ MiniC02/Enums/AcessibilityTagEnum.swift
@@ -0,0 +1,23 @@
+//
+//  AcessIconsEnum.swift
+//  MiniC02
+//
+//  Created by Gabriel Sabaini on 09/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+//enum AccessibilityTag: String, CaseIterable, Identifiable {
+//    case ClosedCaptions = "icon-1"
+//    case InternationalDeaf = "icon-2"
+//
+//    var label: String {
+//        switch self {
+//        case .ClosedCaptions: return "Closed Caption"
+//        case .InternationalDeaf: return "International Deficiencia Auditiva"
+//        }
+//    }
+//
+//    var id: String { return self.rawValue }
+//}
