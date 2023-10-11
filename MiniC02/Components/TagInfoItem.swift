8e32a0172b3d1ca518aaaf4e5c4b85a98beb6672
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2023-10-11 14:09:39 -0300

    Adicionar a tela das tags funcionando :)

--- MiniC02/Components/TagInfoItem.swift
+++ MiniC02/Components/TagInfoItem.swift
@@ -8,26 +8,23 @@
 import SwiftUI
 
 struct TagInfoItem: View {
+    var tag: AccessibilityTag
+    
     var body: some View {
-        
         VStack {
             HStack(alignment: .center){
-                Image(.iconInterpreteLibras)
+                Image(tag.rawValue)
                     .resizable()
                     .frame(width:43, height: 43)
                     .padding(.leading)
                     .padding(.trailing, 10)
                 
                 VStack(alignment: .leading){
-                    Text("O local oferece ajuda e/ou acesso para surdos.")
+                    Text(tag.desc)
                         .font(Font.custom("SF Pro", size: 15))
                         .padding(.trailing)
                 }
             }
         }
     }
-}
-
-#Preview {
-    TagInfoItem()
 }
--- MiniC02/Views/AcessibilityTagInformationView.swift
+++ MiniC02/Views/AcessibilityTagInformationView.swift
@@ -11,15 +11,17 @@ import SwiftUI
 
 struct AcessibilityTagInformationView: View {
     @Environment(\.dismiss) var dismiss
-    var descTagInfo : [String] = ["O local oferece ajuda e/ou acesso para surdos.", "O local possui sistema de aro magnético e o sistema de som será transmitido no aparelho auditivo ou implante coclear. Basta colocar no modo 'T' (telecoil)..", "No local há alguma forma de tecnologia que fornece acessibilidade.", "A programação televisiva ou um vídeo possui legendas ocultas que podem ser ativadas.", "Legendas que já vêm no vídeo e não podem ser desativadas.", "Aparelho de telefone possui um controle de volume amplificado que pode ser usado por pessoas que possuem perda auditiva leve ou moderada.", "O local possui um telefone para surdos que possui um teclado e visor que permitem enviar e receber mensagens.", "O barulho ambiente é alto demais e pode provocar perda auditiva ou dano.", "Para profissionais intérpretes que trabalham e atuam na comunicação em Libras.", "O local terá pessoas que se comunicam em libras."]
     
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(spacing: 25) {
-                    ForEach(descTagInfo.indices, id: \.self){ index in
-                        TagInfoItem()
+                VStack(alignment: .leading, spacing: 25) {
+                    ForEach(AccessibilityTag.allCases, id: \.self) { tag in
+                        TagInfoItem(tag: tag)
+                            .padding(.horizontal, 15)
                     }
+                    
+                   
                 }
                 .toolbar {
                     ToolbarItem(placement: .principal) {
