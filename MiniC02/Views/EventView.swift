47dd3ce6ec9976e7f0e6de1c81c1d97499ed5910
gabrielsabaini <gabriel.tesch@gmail.com>
2023-10-16 21:06:37 -0300

    Após o Merge

--- MiniC02/Views/EventView.swift
+++ MiniC02/Views/EventView.swift
@@ -9,92 +9,88 @@ import SwiftUI
 import Aptabase
 
 struct EventView: View {
-	
-	var event: EventModel
-	@Binding var salvo: Bool
-	@Binding var confirmed: Bool
-	@State private var oneOpen: Bool = false
-	@State private var twoOpen: Bool = false
-	@State var showingInfoView: Bool = false
-	@State var tag: AccessibilityTag
-	
-	@Environment(\.dismiss) private var dismiss
-	
-	@EnvironmentObject var vm : ViewModel
-	
-	var body: some View {
-		NavigationStack {
-			ScrollView{
-				
-				VStack(alignment: .leading){
-					Image(event.imagename)
-						.resizable()
-						.scaledToFit()
-					
-					VStack(alignment: .leading){
-						
-						EventHeaderWithSave
-						
-						Text(event.title)
-							.font(Font.custom("SF Pro", size: 28))
-							.padding(.top, 14)
-							.bold()
-						
-						EventDescription
-							.padding(.vertical)
-						
-						EventAcessibility
-						
-						
-						EventInfo
-							.padding(.vertical)
-						
-						
-						
-						Button {
-							
-							if !confirmed {
-								vm.saveConfirmedEventToProfile(event: event)
-							} else if confirmed {
-								vm.unsaveConfirmedEventFromProfile(event: event)
-							}
-							
-							confirmed.toggle()
-							
-						} label: {
-							if !confirmed {
-								Text("Eu vou")
-							} else {
-								Text("Confirmado!")
-							}
-						Button("Eu vou") {
-							print("eu vou")
-                            Aptabase.shared.trackEvent("Botao Eu Vou", with: ["Eventos": event.title])
-						}
-						.buttonStyle(PlainButtonStyle())
-						.padding(.vertical, 20)
-						
-					}
-					.padding(.leading, 14)
-					
-				}
-			}
-			.toolbar {
-				ToolbarItem(placement: .cancellationAction) {
-					Button(action: {
-						dismiss()
-					}, label: {
-						HStack {
-							Image(systemName: "chevron.left")
-							Text("Início")
-						}
-					})
-					.foregroundStyle(Color("DarkBlue"))
-				}
-			}
-		}
-		.navigationBarBackButtonHidden(true)
-	}
+    
+    var event: EventModel
+    @Binding var salvo: Bool
+    @Binding var confirmed: Bool
+    @State private var oneOpen: Bool = false
+    @State private var twoOpen: Bool = false
+    @State var showingInfoView: Bool = false
+    @State var tag: AccessibilityTag
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @EnvironmentObject var vm : ViewModel
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView{
+                
+                VStack(alignment: .leading){
+                    Image(event.imagename)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    VStack(alignment: .leading){
+                        
+                        EventHeaderWithSave
+                        
+                        Text(event.title)
+                            .font(Font.custom("SF Pro", size: 28))
+                            .padding(.top, 14)
+                            .bold()
+                        
+                        EventDescription
+                            .padding(.vertical)
+                        
+                        EventAcessibility
+                        
+                        
+                        EventInfo
+                            .padding(.vertical)
+                        
+                        
+                        
+                        Button {
+                            
+                            if !confirmed {
+                                vm.saveConfirmedEventToProfile(event: event)
+                                Aptabase.shared.trackEvent("Botao Eu Vou", with: ["Eventos": event.title])
+                            } else if confirmed {
+                                vm.unsaveConfirmedEventFromProfile(event: event)
+                                Aptabase.shared.trackEvent("Botao Desconfirmou", with: ["Eventos": event.title])
+                            }
+                            
+                            confirmed.toggle()
+                            
+                        } label: {
+                            if !confirmed {
+                                Text("Eu vou")
+                            } else {
+                                Text("Confirmado!")
+                            }
+                        }
+                        .padding(.leading, 14)
+                        
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button(action: {
+                            dismiss()
+                        }, label: {
+                            HStack {
+                                Image(systemName: "chevron.left")
+                                Text("Início")
+                            }
+                        })
+                        .foregroundStyle(Color("DarkBlue"))
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+    }
 }
 
 //#Preview {
@@ -102,97 +98,97 @@ struct EventView: View {
 //}
 
 extension EventView {
-	
-	private var EventHeaderWithSave: some View {
-		HStack{
-			Image(event.hostname)
-			
-			Text(event.hostname)
-				.font(Font.custom("SF Pro", size: 17))
-				.foregroundStyle(Color("MainTextColor"))
-			
-			Spacer()
-			
-			Button {
-				
-				if !salvo {
-					vm.saveEventToProfile(event: event)
-                    Aptabase.shared.trackEvent("Botao Salvar", with: ["Eventos": event.title])
-				}
-				else if salvo {
-					vm.unsaveEventFromProfile(event: event)
-                    Aptabase.shared.trackEvent("Botao Dessalvar", with: ["Eventos": event.title])
-				}
-				
-				withAnimation(.linear(duration: 0.3)) {
-					salvo.toggle()
-				}
-				
-			} label: {
-				
-				
-				if salvo{
-					Image(systemName: "bookmark.fill")
-						.font(.title2)
-						.padding(.trailing, 14)
-						.foregroundColor(Color("DarkYellow"))
-				}
-				else {
-					Image(systemName: "bookmark")
-						.font(.title2)
-						.padding(.trailing, 14)
-						.foregroundColor(Color("DarkYellow"))
-				}
-				
-			}
-		}
-		.padding(.bottom, 0)
-	}
-	
-	private var EventInfo: some View {
-		VStack(alignment: .leading) {
-			Text("Informações")
-				.font(Font.custom("SF Pro Text", size: 17)
-					.weight(.semibold))
-				.foregroundColor(Color(red: 0.59, green: 0.59, blue: 0.59))
-			
-			HStack{
-				Image(systemName: "calendar")
-					.foregroundStyle(Color("DarkBlue"))
-				
-				Text(event.date)
-					.foregroundStyle(Color("MainTextColor"))
-				Image(systemName: "clock")
-					.foregroundStyle(Color("DarkBlue"))
-				
-				Text(event.time)
-					.foregroundStyle(Color("MainTextColor"))
-			}
-			.padding(.vertical)
-			HStack{
-				Image(systemName: "mappin")
-					.foregroundStyle(Color("DarkBlue"))
-				
-				Text(event.location + " - " + event.neighborhood)
-					.foregroundStyle(Color("MainTextColor"))
-			}
-		}
-		.padding(.top)
-	}
-	
-	private var EventDescription: some View {
-		VStack(alignment: .leading) {
-			Text("Descrição")
-				.font(Font.custom("SF Pro Text", size: 17)
-					.weight(.semibold))
-				.foregroundColor(Color("DarkGray"))
-				.padding(.bottom, 5)
-			
-			Text(event.desc)
-				.font(Font.custom("SF Pro", size: 17))
+    
+    private var EventHeaderWithSave: some View {
+        HStack{
+            Image(event.hostname)
+            
+            Text(event.hostname)
+                .font(Font.custom("SF Pro", size: 17))
                 .foregroundStyle(Color("MainTextColor"))
-		}
-	}
+            
+            Spacer()
+            
+            Button {
+                
+                if !salvo {
+                    vm.saveEventToProfile(event: event)
+                    Aptabase.shared.trackEvent("Botao Salvar", with: ["Eventos": event.title])
+                }
+                else if salvo {
+                    vm.unsaveEventFromProfile(event: event)
+                    Aptabase.shared.trackEvent("Botao Dessalvar", with: ["Eventos": event.title])
+                }
+                
+                withAnimation(.linear(duration: 0.3)) {
+                    salvo.toggle()
+                }
+                
+            } label: {
+                
+                
+                if salvo{
+                    Image(systemName: "bookmark.fill")
+                        .font(.title2)
+                        .padding(.trailing, 14)
+                        .foregroundColor(Color("DarkYellow"))
+                }
+                else {
+                    Image(systemName: "bookmark")
+                        .font(.title2)
+                        .padding(.trailing, 14)
+                        .foregroundColor(Color("DarkYellow"))
+                }
+                
+            }
+        }
+        .padding(.bottom, 0)
+    }
+    
+    private var EventInfo: some View {
+        VStack(alignment: .leading) {
+            Text("Informações")
+                .font(Font.custom("SF Pro Text", size: 17)
+                    .weight(.semibold))
+                .foregroundColor(Color(red: 0.59, green: 0.59, blue: 0.59))
+            
+            HStack{
+                Image(systemName: "calendar")
+                    .foregroundStyle(Color("DarkBlue"))
+                
+                Text(event.date)
+                    .foregroundStyle(Color("MainTextColor"))
+                Image(systemName: "clock")
+                    .foregroundStyle(Color("DarkBlue"))
+                
+                Text(event.time)
+                    .foregroundStyle(Color("MainTextColor"))
+            }
+            .padding(.vertical)
+            HStack{
+                Image(systemName: "mappin")
+                    .foregroundStyle(Color("DarkBlue"))
+                
+                Text(event.location + " - " + event.neighborhood)
+                    .foregroundStyle(Color("MainTextColor"))
+            }
+        }
+        .padding(.top)
+    }
+    
+    private var EventDescription: some View {
+        VStack(alignment: .leading) {
+            Text("Descrição")
+                .font(Font.custom("SF Pro Text", size: 17)
+                    .weight(.semibold))
+                .foregroundColor(Color("DarkGray"))
+                .padding(.bottom, 5)
+            
+            Text(event.desc)
+                .font(Font.custom("SF Pro", size: 17))
+                .foregroundStyle(Color("MainTextColor"))
+        }
+    }
     
     private var EventAcessibility: some View {
         VStack(alignment: .leading){
